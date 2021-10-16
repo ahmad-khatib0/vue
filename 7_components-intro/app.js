926bb36da484270cda68ed1e9284d2c556c2b62b
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-16 10:43:56 -0700

    how to creaete or rejister a component in vue , and use it

--- 7_components-intro/app.js
+++ 7_components-intro/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "manuel",
@@ -18,12 +17,38 @@ const app = Vue.createApp({
       ],
     };
   },
+  computed: {},
+});
+
+app.component("friends-contact", {
+  template: `
+  <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone: </strong>{{ friend.phone }}</li>
+        <li><strong>Email: </strong>{{ friend.email }}</li>
+      </ul>
+    </li>`,
+  data() {
+    return {
+      friend: {
+        id: "manuel",
+        name: "manuel lorenz",
+        phone: "333-222-3445",
+        email: "manuel@gmail.com",
+      },
+      detailsAreVisible: false, //if you have the same name inside
+      // createApp config as here , that ok . no clash will be
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
-  computed: {},
 });
 
 app.mount("#app");
