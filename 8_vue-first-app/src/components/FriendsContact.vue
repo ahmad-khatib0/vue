2580c5661ac328555cd2de62fbd528b7ed85e926
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-16 22:47:43 -0700

    installed the first app via vue-cli, and prepare it to go depeer about components

---
+++ 8_vue-first-app/src/components/FriendsContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <h2>{{ friend.name }}</h2>
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "manuel lorenz",
+        email: "manuel@localhost.com",
+        phone: "224-333-2222",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ 8_vue-first-app/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import FriendsContact from "./components/FriendsContact.vue";
+const app = createApp(App);
+app.component("friends-contact", FriendsContact);
+app.mount("#app");
