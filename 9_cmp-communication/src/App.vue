8ba3ebf00042d73aec981dce679cbf6e509c5532
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-19 12:55:59 -0700

    use provide inject with methods/functions

--- 9_cmp-communication/src/App.vue
+++ 9_cmp-communication/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -42,6 +42,7 @@ export default {
     //provide is like passing data like props
     return {
       topics: this.topics,
+      selectTopic: this.activateTopic,
     };
   },
 
--- 9_cmp-communication/src/components/KnowledgeGrid.vue
+++ 9_cmp-communication/src/components/KnowledgeGrid.vue
@@ -6,7 +6,6 @@
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
-      @select-topic="$emit('select-topic', $event)"
     ></knowledge-element>
   </ul>
 </template>
@@ -15,6 +14,5 @@
 export default {
   inject: ['topics'],
   //  inject to retrieve these passed data
-  emits: ['select-topic'],
 };
 </script>
