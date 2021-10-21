d4c9145a40fd421a6e92ca050e39745d8b3dc1dc
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-22 00:10:05 +0300

    more about components , and teleporting in vue, which used to render an element in specific place in dom

--- 10_components-advenced/src/components/ManageGoals.vue
+++ 10_components-advenced/src/components/ManageGoals.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
     <h2>manage goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <!-- render it in the body  -->
+      <error-alert v-if="inputIsInvalid">
+        <h1>input is invalid</h1>
+        <p>please enter at least a fev characters...</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script>
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") this.inputIsInvalid = true;
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
 
 <style></style>
