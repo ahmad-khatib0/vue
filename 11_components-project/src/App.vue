4c2d1d8a9b6bc585e74cb02941131da68acf2510
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-23 09:43:19 +0300

    switch between taps

--- 11_components-project/src/App.vue
+++ 11_components-project/src/App.vue
@@ -1,31 +1,19 @@
 <template>
   <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 // import { v4 as uuidv4 } from 'uuid';
 export default {
   components: {
     TheHeader,
+    TheResources,
   },
   data() {
-    return {
-      storedResources: [
-        {
-          id: 'officialGuide',
-          title: 'Official Guide',
-          description: 'the official vue js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Goggle',
-          description: 'Learn how to goggle things',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
--- 11_components-project/src/components/learning-resources/TheResources.vue
+++ 11_components-project/src/components/learning-resources/TheResources.vue
@@ -1,14 +1,20 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTap('stored-resources')"
+    <base-button
+      @click="setSelectedTap('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTap('add-resource')"
+    <base-button
+      @click="setSelectedTap('add-resource')"
+      :mode="addResButtonMode"
       >Add Resource
     </base-button>
   </base-card>
 
   <component :is="selectedTap"></component>
+  <!-- to understand this better,think of it as the component element will be  -->
+  <!-- replaced either by <storedResources/> or <AddResource /> instead -->
 </template>
 
 <script>
@@ -21,7 +27,34 @@ export default {
   data() {
     return {
       selectedTap: 'stored-resources',
+      storedResources: [
+        {
+          id: 'officialGuide',
+          title: 'Official Guide',
+          description: 'the official vue js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Goggle',
+          description: 'Learn how to goggle things',
+          link: 'https://google.com',
+        },
+      ],
     };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTap === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTap === 'add-resource' ? null : 'flat';
+    },
   },
   methods: {
     setSelectedTap(tap) {
