11284465a9eb6b6c1ebea5ffd2985f929621cf74
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-23 11:13:48 +0300

    passing a method to a children with help of provide

--- 11_components-project/src/components/learning-resources/TheResources.vue
+++ 11_components-project/src/components/learning-resources/TheResources.vue
@@ -1,44 +1,47 @@
 <template>
   <base-card>
     <base-button
-      @click="setSelectedTap('stored-resources')"
+      @click="setSelectedTab('stored-resources')"
       :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
     <base-button
-      @click="setSelectedTap('add-resource')"
+      @click="setSelectedTab('add-resource')"
       :mode="addResButtonMode"
-      >Add Resource
-    </base-button>
+      >Add Resource</base-button
+    >
   </base-card>
-
-  <component :is="selectedTap"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
   <!-- to understand this better,think of it as the component element will be  -->
   <!-- replaced either by <storedResources/> or <AddResource /> instead -->
 </template>
 
 <script>
-import BaseButton from '../UI/BaseButton.vue';
-import BaseCard from '../UI/BaseCard.vue';
 import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
 
 export default {
-  components: { BaseButton, BaseCard, StoredResources },
+  components: {
+    StoredResources,
+    AddResource,
+  },
   data() {
     return {
-      selectedTap: 'stored-resources',
+      selectedTab: 'stored-resources',
       storedResources: [
         {
-          id: 'officialGuide',
+          id: 'official-guide',
           title: 'Official Guide',
-          description: 'the official vue js documentation',
+          description: 'The official Vue.js documentation.',
           link: 'https://vuejs.org',
         },
         {
           id: 'google',
-          title: 'Goggle',
-          description: 'Learn how to goggle things',
-          link: 'https://google.com',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
         },
       ],
     };
@@ -46,22 +49,31 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
     };
   },
   computed: {
     storedResButtonMode() {
-      return this.selectedTap === 'stored-resources' ? null : 'flat';
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
     },
     addResButtonMode() {
-      return this.selectedTap === 'add-resource' ? null : 'flat';
+      return this.selectedTab === 'add-resource' ? null : 'flat';
     },
   },
   methods: {
-    setSelectedTap(tap) {
-      this.selectedTap = tap;
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title,
+        description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     },
   },
 };
 </script>
-
-<style></style>
