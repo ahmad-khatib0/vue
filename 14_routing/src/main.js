725f2623bba7647ede57e5a0c4810d0495109e40
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-10-24 11:47:18 +0300

    redirecting and not found page

--- 14_routing/src/main.js
+++ 14_routing/src/main.js
@@ -5,15 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    // { path: '/', redirect: '/teams' }, or with alias
+    { path: '/teams', component: TeamsList, alias: '/' }, //but alias don't update path
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
     // it tells the view router that the dynamic parameters should be passed into
     //  this component as props rather than just on the $route property.
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active', // to replace the router-link-active with this
 });
