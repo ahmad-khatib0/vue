0c364c900a75c066227bd76084f84bbfb0fc01b8
AHMAD_KHATIB <Ahmadkhatib0@gmail.com>
2021-11-01 00:04:26 +0300

    buillding the coach detail component

--- 17_find-coach-project/src/pages/coaches/CoachesList.vue
+++ 17_find-coach-project/src/pages/coaches/CoachesList.vue
@@ -1,29 +1,32 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.last - name"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches added yet</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches added yet</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
 export default {
-  components: { CoachItem },
+  components: { CoachItem, BaseButton },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
