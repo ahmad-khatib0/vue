07fddba451d3d4aa78e339b517eada5cd9458209
AHMADKHATIB <Ahmadkhatib0@gmail.com>
2021-11-04 22:26:20 +0300

    add the login  functionalty with firebase

--- 18_authentiction/src/store/modules/coaches/actions.js
+++ 18_authentiction/src/store/modules/coaches/actions.js
@@ -9,8 +9,10 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://coach-app-1b7f2-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://coach-app-1b7f2-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
