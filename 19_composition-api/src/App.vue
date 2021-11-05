1b23423d27fc1573fedb508404438645a395c8c0
AHMADKHATIB <Ahmadkhatib0@gmail.com>
2021-11-05 19:59:51 +0300

    replacing the data with setup , and ref and reactive in vue

--- 19_composition-api/src/App.vue
+++ 19_composition-api/src/App.vue
@@ -1,16 +1,57 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <!-- <h2>{{ name }}</h2>
+    <h3>{{ age }}</h3>  method 1-->
+    <!-- <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>  method 2 -->
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
 <script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
+  setup() {
+    // const UName = ref('Maximilian'); method
+    // const userAge = ref(44);
+    // under the hood, it creates an object. This string is stored in that object
+    // and Vue will watch this object, so that whenever we assign a new value instead
+    // of that original string.Vue will be aware of it and is able to update the DOM.
+
+    // setTimeout(() => {
+    //   UName.value = 'Ahmad';
+    //   userAge.value = 22;
+    // }, 2000);
+    // return { userName: UName, age: userAge }; method 1 end
+
+    // const user = ref({    //method 2
+    //   name: 'Maximilian ',
+    //   age: 33,
+    // });
+    // setTimeout(() => {
+    //   user.value.name = 'Ahmad';
+    //   user.value.age = 23;
+    // }, 2000);
+    // return { user };  //method 2 end
+
+    const user = reactive({
+      //method 3, reactive works just with objects, while ref works with all
+      name: 'Maximilian ',
+      age: 33,
+    });
+    setTimeout(() => {
+      user.name = 'Ahmad';
+      user.age = 23;
+    }, 2000);
+    return { user }; //method 3 end
   },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
 };
 </script>
 
