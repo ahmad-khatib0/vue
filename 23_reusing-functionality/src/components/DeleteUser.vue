4f4ba45120284efc1900baae1c2f2c1d5307d486
AHMADKHATIB <Ahmadkhatib0@gmail.com>
2021-11-06 16:44:18 +0300

    overriding in mixin , and global mixins

--- 23_reusing-functionality/src/components/DeleteUser.vue
+++ 23_reusing-functionality/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -13,6 +13,12 @@ import UserAlert from '../components/UserAlert.vue';
 import alertMixin from '../mixins/alert';
 export default {
   mixins: [alertMixin],
+  data() {
+    return {
+      alertTitle: 'Wanna delete user? ',
+      // alertIsVisible: true, //this will override what in side the mixin
+    };
+  },
   components: {
     UserAlert,
   },
